4ec0b42109f2a790289ad389e39923835ccc86b7
Ryan Novotny <ryan.novotny@gmail.com>
2016-11-24 15:16:35 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+/* jshint asi: true */
+jQuery( document ).ready(function() {
+	'use strict'
+	//window.document.designMode = "On";
+	$('#button-holder').draggable()
+	
+	$('form').click( function(e){
+	
+		var newPosition = $(e.target).position()
+		newPosition.left = newPosition.left + $('form').outerWidth() + 5
+		if ( newPosition.left > 0 ) {
+			$('#button-holder').animate( newPosition )
+		}
+		
+	})
+	
+	$('.btn').click(function(){
+		document.execCommand($(this).attr('id'), false, null )
+	})
+})
+
